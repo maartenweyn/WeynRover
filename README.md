e5246d8fb105bc6dd1216891f4c556020e9ffa70
Maarten Weyn <maarten@wesdec.be>
2020-04-16 12:49:05 +0200

    clean up code + comments

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # WeynRover
 
+##Hardware coponents
 
-Sabertooth Info: https://wiki.dfrobot.com/Sabertooth_Dual_10A_DC_Motor_Driver__SKU__DRI0003_
+* Rover body: [Lynxmotion A4WD1] (http://www.lynxmotion.com/p-603-aluminum-4wd1-rover-kit.aspx)
+* Motor controller: [Sabertooth 2x12] (https://www.dimensionengineering.com/products/sabertooth2x12)
+* Sonar Sensor: [Parallax Ping)))] (https://www.parallax.com/sites/default/files/downloads/28015-PING-Sensor-Product-Guide-v2.0.pdf)
 
 
-BLE control apps
-- ArduinoRC
-- Arduino & ESP32 Bluetoot Controller Appl -Dabble
-- https://play.google.com/store/apps/details?id=iyok.com.blejoystick
+
--- arduino/weynrover/weynrover.ino
+++ arduino/weynrover/weynrover.ino
@@ -1,71 +1,102 @@
+    
+/*
+  WeynRover
+  
+  Basic control software for the an ESP32 connected to a Sabertooth motor driver and Ping Sonar Sensor
+
+  The circuit:
+  * IO12: Data pin of Parallax Sonar Sensor
+  * IO15 (=UART2 TX): S1 of Sabertooth 2x12 motor driver
+
+  April 2020
+  BMaarten Weyn - Weyn.Tech
+
+  https://github.com/maartenweyn/WeynRover
+
+*/
+
+
 #include "BLESerial.h"
 #include <NewPing.h>
 
-#define TRIGGER_PIN  12
-#define ECHO_PIN     12
-#define MAX_DISTANCE 200
-#define CHECK_SONAR_INTERVAL 50
-#define DISTANCE_TO_CLOSE 10
+#define SONAR_TRIGGER_PIN  12
+#define SONAR_ECHO_PIN     12 // Parallax Ping sensor is a 1 pin sonar
+#define SONAR_MAX_DISTANCE 200
 
 
-static BLESerial bleSerial;
-static int speedLeft = 0;
-static int speedRight = 0;
-static bool isTurning = false;
-unsigned long pingTimer = 0;
-static bool useSonar = true;
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+#define CHECK_SONAR_INTERVAL 50 // Every 50 ms we check the distance measured by the Sonar sensor
+#define DISTANCE_TO_CLOSE 10    // When we are closer then 10 cm we will stop
 
 
+
+
+static int speedLeft = 0;       // Speed left motor  (-63 to 63)
+static int speedRight = 0;      // Speed right motor (-63 to 63)
+
+static bool isTurning = false;  // Flag to indication if the left and right motors are not equal (hence turning)
+
+static unsigned long pingTimer = 0;    // Timer to keep track of the next done sonar ping
+static bool useSonar = true;    // Flag to indicate if we will use the sonar
+
+static BLESerial bleSerial;     // The handle for Bluetooth Low Energy Serial service
+static NewPing sonar(SONAR_TRIGGER_PIN, SONAR_ECHO_PIN, SONAR_MAX_DISTANCE); // Handle for the sonar
+
+
+// Initialization
 void setup() {
-  // put your setup code here, to run once:
-
-  Serial.begin(115200);
-  Serial2.begin(38400); // link to Sabertoot 2x12
-  bleSerial.begin("Rover"); //Bluetooth device name
-  pingTimer = millis() + CHECK_SONAR_INTERVAL;
+  Serial.begin(115200);     // Initialize serial for debugging
+  Serial2.begin(38400);     // Initialize the TX UART for the Sabertoot 2x12 - Using UART2 TX, hense IO15
+  
+  bleSerial.begin("Rover"); //start BLE Serial with  device name
+  
+  pingTimer = millis() + CHECK_SONAR_INTERVAL;   // Initialize next sonar ping
 }
 
 
+void setRoverSpeed(int left, int right) {
+  Serial.printf("New speed %d, %d\n", left, right);
+      
+  Serial2.write(64 + left);
+  delay(1);
+  Serial2.write(192 + right);
+}
 
+// Main loop
 void loop() {
-    static bool bleConnected = false;
-    static bool toClose = false;
-    bool changeSpeed = false;
+    static bool bleConnected = false; // persitent flag with connecton status
+    static bool toClose = false;      // persistent flag to indicate if we are to close to an object and should not move forward
     
+    bool changeSpeed = false; //flag to indicate if we have to change the moter speed (e.g. on an incomming new command)
+
+    // Print if BLE connection is changed and stop rover if connection is gone
     if (bleSerial.connected()) {
-        if (!bleConnected) {
+        if (!bleConnected) { // if not connected
             Serial.println("BLE Connected");
             bleConnected = true;
         }
     } else {
-        if (bleConnected) {
+        if (bleConnected) { // if connected
             Serial.println("BLE Disconnected");
             bleConnected = false;
+            
             // Stop rover if no controller is connected
             speedLeft = 0;
             speedRight = 0;
-            Serial2.write(64 + speedLeft);
-            delay(1);
-            Serial2.write(192 + speedRight);
+            setRoverSpeed(speedLeft, speedRight);
         }
     }
 
-   if (useSonar) {
-     if (millis() >= pingTimer) {
-        pingTimer += CHECK_SONAR_INTERVAL;
+   if (useSonar) { // if we are using the sonar
+     if (millis() >= pingTimer) { // check if we need to measure 
+        pingTimer += CHECK_SONAR_INTERVAL; // set new timestamp when to measure
         
         int distance = sonar.ping_cm();
-        if (distance > 1) {
-          toClose = distance < DISTANCE_TO_CLOSE;
+        if (distance > 1) { // if range > max it  will give a range of 0, some errornous measurements of 1
+          toClose = distance < DISTANCE_TO_CLOSE; // if object it to close
           
-          Serial.print("Ping: ");
-          Serial.print(distance);
-          Serial.print("cm ");
+          Serial.printf("Ping: %d cm: %d\n", distance, toClose);
           
-          Serial.println(toClose);
-          if (toClose) changeSpeed = true;
+          if (toClose) changeSpeed = true; // if we are to close flag that we have to change the moter speed
         } else {
           toClose = false;
         }
@@ -73,44 +104,45 @@ void loop() {
    }
 
 
-  // put your main code here, to run repeatedly:
+  // if new data is comming from the BLE serial
+  // format of the data
+  // byte 1: datatype
+  //  - 1 == direction command in byte 2
+  //  - 2 == options in byte 2 and 3 (e.g. sonar on/off 
   if (bleSerial.available()) {
+    byte commandType = bleSerial.read(); // read first byte
+    Serial.printf("commandType: %d\n", commandType);
     
-    byte commandType = bleSerial.read();
-    Serial.print("commandType: ");
-    Serial.println(commandType);
-    
-    if (commandType == 1) { // Direction
-      char command = (char)bleSerial.read();
+    if (commandType == 1) { // Direction command
+      char command = (char) bleSerial.read(); //read direction command as second byte
       switch (command) {
         case 'f': // Front
-          if (isTurning) {
+          if (isTurning) { // if rover was turing go straight ahead again with average speed of the 2 motors
             speedLeft = (speedLeft + speedRight) / 2;
             speedRight = speedLeft;
             isTurning = false;
-          } else {
+          } else { // otherwise increase both motor speeds
             speedLeft += 5;
             speedRight += 5;
-            if (speedLeft > 63) speedLeft = 63;
+            if (speedLeft > 63) speedLeft = 63;  // limit the range to max 63
             if (speedRight > 63) speedRight = 63;
           }
           Serial.print("F: ");
           break;
         case 'b': // Back
-          if (isTurning) {
+          if (isTurning) { // if rover was turing go straight backwards again with average speed of the 2 motors
             speedLeft = (speedLeft + speedRight) / 2;
             speedRight = speedLeft;
             isTurning = false;
-          } else {
+          } else { // otherwise decrease both motor speeds
             speedLeft -= 5;
             speedRight -= 5;
-            if (speedLeft < -63) speedLeft = -63;
+            if (speedLeft < -63) speedLeft = -63;   // limit the range to max -63
             if (speedRight < -63) speedRight = -63;
           }
-          
           Serial.print("B: ");
           break;
-        case 'l': // Left
+        case 'l': // Left - decrese speed left motor, increase speed of the right motor
           speedLeft -= 5;
           speedRight += 5;
           if (speedLeft < -63) speedLeft = -63;
@@ -118,7 +150,7 @@ void loop() {
           isTurning = true;
           Serial.print("L: ");
           break;
-        case 'r': // Right
+        case 'r': // Right - decrese speed right motor, increase speed of the left motor
           speedLeft += 5;
           speedRight -= 5;
           if (speedLeft > 63) speedLeft = 63;
@@ -126,54 +158,42 @@ void loop() {
           isTurning = true;
           Serial.print("R: ");
           break;
-        case '-':
+        case '-': // stop
           speedLeft = 0;
           speedRight = 0;
           Serial.print("-: ");
           break;
         default:
-          Serial.print("Unknown command: ");
-          Serial.write(command);
-          Serial.println("");
+          Serial.printf("Unknown command: %s\n", command);
           break;
       }
 
-       changeSpeed = true;
-    } else if (commandType == 2) // options
-    {
+       changeSpeed = true; // set flag to update motor speed
+    } else if (commandType == 2) {// options
       byte value  = bleSerial.read();
-      Serial.print("value 1: ");
-      Serial.println(value);
+      Serial.printf("value 1: %d\n", value);
     
-      if (value == 1) {
+      if (value == 1) { // sonar on/off
         value  = bleSerial.read();
-        Serial.print("value 2: ");
-        Serial.println(value);
+        Serial.printf("value 2: %d\n", value);
         if (value == 0) {
           useSonar = false;
-          Serial.print("sonar off");
+          Serial.println("sonar off");
         } else {
           useSonar = true;
-          Serial.print("sonar on");
+          Serial.println("sonar on");
         }
       }
     }
   }
 
-  if (changeSpeed) {
-    if (toClose) {
-      if (speedLeft + speedRight > 0) {
+  if (changeSpeed) { // if motor speed has to be changed
+    if (toClose) { // if to close
+      if (speedLeft + speedRight > 0) { // only stop motors if we are going forward
         speedLeft = 0;
         speedRight = 0;
       }
     }
-
-    Serial.print(speedLeft);
-    Serial.print(" ");
-    Serial.println(speedRight);
-
-    Serial2.write(64 + speedLeft);
-    delay(1);
-    Serial2.write(192 + speedRight); 
+    setRoverSpeed (speedLeft, speedRight);
   }
 }
